f97b95b9db12cb8f4214eae7285e13f867c0c4ba
Nenad Prahovljanovic <nenad@getidee.de>
2019-10-13 16:31:48 +0200

    Implement UI for displaying data.
    A UITableView with custom cell for displaying users’ data.

--- SPDF/Controllers/ViewController.swift
+++ SPDF/Controllers/ViewController.swift
@@ -8,13 +8,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController{
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    private var users: [User] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        getUsers()
+    }
+    
+    private func getUsers() {
+        UserManager.shared.fetchUsers(page: 1, count: 5) { (users) in
+            self.users = users ?? []
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
     }
 
-
 }
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "UserTableViewCell"
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! UserTableViewCell
+        
+        cell.title.text = "\(users[indexPath.row].firstName) \(users[indexPath.row].lastName)"
+        
+        cell.subtitle.text = "Age \(users[indexPath.row].age)"
+        
+        UserManager.shared.getUserImage(imageUrl: users[indexPath.row].imageThumb, completion: { (image) in
+            DispatchQueue.main.async {
+                cell.userImage.image = image
+            }
+        })
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print("Users count \(users.count)")
+        return users.count
+    }
+    
+}
+
 
---
+++ SPDF/Views/Cells/UserTableViewCell.swift
@@ -0,0 +1,34 @@
+//
+//  UserTableViewCell.swift
+//  SPDF
+//
+//  Created by Nenad Prahovljanovic on 10/13/19.
+//  Copyright © 2019 Nenad Prahovljanovic. All rights reserved.
+//
+
+import UIKit
+
+class UserTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var subtitle: UILabel!
+    @IBOutlet weak var userImage: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    override func prepareForReuse() {
+        title.text = nil
+        subtitle.text = nil
+        userImage.image = nil
+    }
+
+}
