b568f47b33253ea949bc0bfc13f546cc2517293d
Nenad Prahovljanovic <nenad@getidee.de>
2019-10-13 09:17:59 +0200

    Project organization. Add Reachability and Gradient view

---
+++ SPDF/Views/GradientView.swift
@@ -0,0 +1,43 @@
+//
+//  GradientView.swift
+//  SPDF
+//
+//  Created by Nenad Prahovljanovic on 10/13/19.
+//  Copyright © 2019 Nenad Prahovljanovic. All rights reserved.
+//
+
+import UIKit
+@IBDesignable
+class GradientView: UIView {
+    @IBInspectable var firstColor: UIColor = UIColor.clear {
+        didSet {
+            updateView()
+        }
+    }
+    @IBInspectable var secondColor: UIColor = UIColor.clear {
+        didSet {
+            updateView()
+        }
+    }
+    @IBInspectable var isHorizontal: Bool = true {
+        didSet {
+            updateView()
+        }
+    }
+    override class var layerClass: AnyClass {
+        get {
+            return CAGradientLayer.self
+        }
+    }
+    func updateView() {
+        let layer = self.layer as! CAGradientLayer
+        layer.colors = [firstColor, secondColor].map {$0.cgColor}
+        if (isHorizontal) {
+            layer.startPoint = CGPoint(x: 0, y: 0.5)
+            layer.endPoint = CGPoint (x: 1, y: 0.5)
+        } else {
+            layer.startPoint = CGPoint(x: 0.5, y: 0)
+            layer.endPoint = CGPoint (x: 0.5, y: 1)
+        }
+    }
+}
